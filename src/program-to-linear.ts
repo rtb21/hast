b37c0f89b2997e6192b83e16e08af78bfb905d2d
Rob Bacon <bacon@cantab.net>
2018-03-15 12:36:11 +0000

    Adding more node types

--- src/program-to-linear.ts
+++ src/program-to-linear.ts
@@ -5,8 +5,12 @@ export interface item {
     type: string;
 }
 
+let unknownElements: { type: string, count: number }[] = [];
+
 function nodeToLinear(input: Node): item[] {
     switch (input.type) {
+        case 'ArrayExpression':
+            return [{ type: 'array expression' }, ...nodesToLinear(input.elements), { type: 'end' }];
         case 'BinaryExpression':
             return [
                 { type: 'binary expression' },
@@ -34,7 +38,7 @@ function nodeToLinear(input: Node): item[] {
             return [{ type: 'expression statement' }, ...nodeToLinear(input.expression)];
         case 'FunctionDeclaration':
             return [ 
-                { type: `function declaration ${input.id}` },
+                { type: `function declaration ${input.id.name}` },
                 input.async ? { type: 'async' } : { type: 'sync' },
                 input.generator ? { type: 'generator' } : { type: 'not generator' },
                 ...nodesToLinear(input.params),
@@ -43,7 +47,9 @@ function nodeToLinear(input: Node): item[] {
             ];
         case 'FunctionExpression':
             return [
-                { type: `function expression ${input.id}` },
+                input.id ? 
+                    { type: `function expression with id ${input.id.name}` } :
+                    { type: 'function expression with no id' },
                 input.async ? { type: 'async' } : { type: 'sync' },
                 input.generator ? { type: 'generator' } : { type: 'not generator' },
                 ...nodesToLinear(input.params), 
@@ -51,19 +57,49 @@ function nodeToLinear(input: Node): item[] {
                 ...nodeToLinear(input.body),
             ];
         case 'Identifier':
-            return [{type: `identifier ${input.name}`}];
+            return [{ type: `identifier ${input.name}` }];
         case 'Literal':
-            return input.
+            return [{ type: `literal ${input.raw}` }, { type: `value ${input.value}`}];
+        case 'LogicalExpression':
+            return [{ type: `logical expression ${input.operator}` }, ...nodeToLinear(input.left), ...nodeToLinear(input.right)];
+        case 'MemberExpression':
+            return [
+                input.computed ?
+                    { type: 'member expression computed' } :
+                    { type: 'member expression' },
+                ...nodeToLinear(input.object),
+                ...nodeToLinear(input.property)
+            ];
+        case 'NewExpression':
+            return [{ type: 'new expression' }, ...nodeToLinear(input.callee), ...nodesToLinear(input.arguments), { type: 'end' }];
         case 'ReturnStatement':
             return input.argument ? [
                 { type: 'return statement' }, ...nodeToLinear(input.argument)
             ] : [
                 { type: 'return statement' }, { type: 'end' }
             ];
+        case 'ThisExpression':
+            return [{ type: 'this expression' }];
         case 'UnaryExpression':
             return [{ type: `unary ${input.prefix ? 'prefix' : 'postfix'} expression `}, ...nodeToLinear(input.argument)];
+        case 'VariableDeclaration':
+            return [{ type: `variable declaration ${input.kind}` }, ...nodesToLinear(input.declarations), { type: 'end' }];
+        case 'VariableDeclarator':
+            return input.init ? [
+                { type: 'variable declarator with init'}, ...nodeToLinear(input.init), ...nodeToLinear(input.id),
+            ] : [
+                { type: 'variable declarator' }, ...nodeToLinear(input.id),
+            ];
         default:
-            console.error(`${input.type} not yet implemented.`);
+            const unknown = unknownElements.find(element => element.type === input.type);
+            if (unknown) {
+                unknown.count += 1;
+            } else {
+                unknownElements.push({
+                    type: input.type,
+                    count: 1
+                });
+            }
             return [];
     }
 }
@@ -73,8 +109,13 @@ function nodesToLinear(input: Node[]): item[] {
 }
 
 export function programToLinear(input: Program): item[] {
-    return [
+    const retVal = [
         ...nodesToLinear(input.body),
         { type: "program end"}
     ];
+    unknownElements.forEach(element => {
+        console.error(`${element.count} of ${element.type} not yet implemented.`);
+    })
+    console.log(`${unknownElements.reduce((count, next) => count + next.count, 0)} of ${unknownElements.length} unknown elements types`);
+    return retVal;
 }
